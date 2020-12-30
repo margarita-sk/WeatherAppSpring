87a79dc7bd7b5a99b127b00f4c54d9f73d7ad4c6
margarita-sk <mskokova2802@gmail.com>
2020-12-30 15:52:35 +0300

    changes in spring security

--- src/main/java/properties/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package properties;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
